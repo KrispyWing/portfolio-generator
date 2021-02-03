27486f5dd06e8361005c8b6cfb87dfa52b04c5d3
Kris Pennimpede <kpennimpede@hotmail.com>
2021-02-02 20:53:22 -0500

    create module to generate HTML file

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i=0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
+const [name, github] = profileDataArgs;
 
-  console.log('================');
 
-  // is the same as this...
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-    
-  };
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
 
-printProfileData(profileDataArgs);
+  console.log('Portfolio Complete! Check out index.html to see the output!');
+});
