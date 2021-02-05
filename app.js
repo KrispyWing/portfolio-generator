fc224a08303aebe819f3e9fe758e14faf35dacf9
Kris Pennimpede <kpennimpede@hotmail.com>
2021-02-04 21:35:13 -0500

    updated html page generation with updated styling for more data and projects

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio Complete! Check out index.html to see the output!');
-// });
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -139,4 +132,11 @@ const promptProject = portfolioData => {
 };
 promptUser()  
   .then(promptProject)
-  .then(portfolioData => console.log(portfolioData));
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw err;
+    });
+  })
